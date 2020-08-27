a4f7bab6826e73ebb8752b39b375c2661b8a7e70
Dmitriy Derepko <xepozz@list.ru>
2020-08-27 22:53:51 +0300

    Add telegram commands config

---
+++ config/telegram/commands.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Api\Telegram\Telegram\Command\StartCommand;
+use App\Api\Telegram\Telegram\Command\SuggestLinkCommand;
+
+return [
+    StartCommand::class,
+    SuggestLinkCommand::class,
+];
